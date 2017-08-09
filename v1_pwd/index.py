b3c0abe45c340e9230da06d38c29cff64cb42bcf
flyotw <flyotw@nowhere.com>
2017-08-09 16:29:17 -0400

    add requests lib and test the input url for validity

--- v1_pwd/index.py
+++ v1_pwd/index.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
+import sys
+import requests
 
 from nousers import construct_unames, construct_passwords
+
+default_url = "http://gjohnson3/pwd"
 
 unames = construct_unames()
 print("constructed {0} usernames for brute forcing...".format(len(unames)))
 
 passwords = construct_passwords()
 print("constructed {0} passwords for brute forcing...".format(len(passwords)))
-prompt = "{0} web requests will be generated.  Do you want to continue?  (Y/n)".format(len(unames) * len(passwords))
+prompt = "{0} web requests will be generated.  Do you want to continue?  (Y/n)  ".format(len(unames) * len(passwords))
 result = input(prompt)
-print(result)
+if not result == "Y": print('quitter!')
+
+r = requests.get(default_url, auth=('admin', 'admin'))
+
+if not r.status_code == 200:
+    print("Exiting:  invalid url {0}").format(default_url)
+    sys.exit(1)
+
+print(r.content)
