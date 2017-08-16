48cccdb03520723b39ba477dea0ba8bb602e2c8c
flyotw <flyotw@nowhere.com>
2017-08-16 09:03:38 -0400

    more password fuzzer updates

--- v1_pwd/v1_password_fuzzer.py
+++ v1_pwd/v1_password_fuzzer.py
@@ -13,17 +13,19 @@ firstname_files = [
 ]
 
 password_files = [
-    "./manual-guesses.txt",
+    #"./manual-guesses.txt",
     #"../wordlists/SecLists/Usernames/Names/top_1000_usa_femalenames_english.txt",
     #"../wordlists/SecLists/Usernames/Names/top_1000_usa_malenames_english.txt",
-    "./v1com.txt",
-    "./manifesto.txt",
-    "./twelveprinciples.txt",
-    "./localinst.txt",
-    "../wordlists/SecLists/Passwords/hak5.txt",
-    "../wordlists/SecLists/Passwords/top_shortlist.txt",
-    "../wordlists/SecLists/Passwords/10_million_password_list_top_10000.txt",
-    "../wordlists/SecLists/Passwords/rockyou-5.txt",
+    #"./v1com.txt",
+    #"./manifesto.txt",
+    #"./twelveprinciples.txt",
+    #"./localinst.txt",
+    #"../wordlists/SecLists/Passwords/hak5.txt",
+    #"../wordlists/SecLists/Passwords/top_shortlist.txt",
+    #"../wordlists/SecLists/Passwords/10_million_password_list_top_10000.txt",
+    #"../wordlists/SecLists/Passwords/rockyou-5.txt",
+    #"../wordlists/SecLists/Passwords/cain.txt",
+    "../wordlists/SecLists/Passwords/best1050.txt"
 ]
 
 raw_last_names = set(utils.read_files_to_list(lastname_files))
@@ -38,7 +40,7 @@ def construct_uname(prepended_char, raw_name, is_uppercase):
 def construct_unames(member_api_url):
     global v1_usernames
     v1_usernames = utils.get_active_usernames(member_api_url)
-    passwords.update(v1_usernames) #check for unchanged default passwords
+    #passwords.update(v1_usernames) #check for unchanged default passwords
     val = v1_usernames
     if not len(raw_last_names) == 0: #build first letter of first name, whole last name set
         for char in alphabet:
