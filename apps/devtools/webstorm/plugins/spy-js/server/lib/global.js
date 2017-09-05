c0ff8b3e3161373306640c6fcf0d892e2a0c1f63
flyotw <flyotw@nowhere.com>
2017-09-05 08:59:35 -0400

    remove heavy devtools - impractical

--- apps/devtools/webstorm/plugins/spy-js/server/lib/global.js
+++
@@ -1,8 +0,0 @@
-/*
- * spy-js - v2.0.0
- * http://spy-js.com
- * Copyright (c) 2013-2016 JetBrains - All Rights Reserved.
- *
- * This source code file is a part of spy-js plugin.
- */
-global._spy_js={},global._spy_js._=require("lodash/dist/lodash.underscore"),_spy_js._.extend=require("lodash").extend,_spy_js._.str=require("underscore.string");
