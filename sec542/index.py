208105aa30c1e7b1684e794134bc6de90d538d6e
flyotw <flyotw@nowhere.com>
2017-09-12 00:00:19 -0400

    index LOOKS correct.  maybe not?  too tired.

--- sec542/index.py
+++ sec542/index.py
@@ -10,17 +10,15 @@ def get_indexed_keywords(sheet):
     print("Processing {0} rows.".format(sheet.nrows))
     for i in range(sheet.nrows):
         # split the tags string
-        wordArray = sheet.cell_value(rowx=i, colx=1).split(",")
+        word_list = sheet.cell_value(rowx=i, colx=1).split(",")
         # get the page number
         pagenum = sheet.cell_value(rowx=i, colx=0)
         # for each word, assign a page
-        for word in wordArray:
-            lowerWord = word.lower().strip()
-            print(lowerWord, pagenum, word in wordArray)
-            if word in wordArray:
-                a = index.get(lowerWord, 'nope')
-            #index[lowerWord] = [pagenum]
-    sortedIndex = sorted(index)
-    print(sortedIndex)
+        for word in word_list:
+            word_lower = word.lower().strip()
+            if index.get(word_lower, None) == None :
+                index[word_lower] = pagenum
+    print(index)
+
 
 get_indexed_keywords(default_sheet)
