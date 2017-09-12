991be8566fa549c1e0c6dfc16b6a580b12015315
flyotw <flyotw@nowhere.com>
2017-09-11 23:16:48 -0400

    baby steps.  print raw data.

--- sec542/index.py
+++ sec542/index.py
@@ -6,20 +6,21 @@ book = xlrd.open_workbook("SEC542.xlsx")
 default_sheet = book.sheet_by_index(0)
 index = {}
 
-
 def get_indexed_keywords(sheet):
     print("Processing {0} rows.".format(sheet.nrows))
     for i in range(sheet.nrows):
         # split the tags string
-        wordlist = sheet.cell_value(rowx=i, colx=1).split(",")
-        print(wordlist)
+        wordArray = sheet.cell_value(rowx=i, colx=1).split(",")
         # get the page number
         pagenum = sheet.cell_value(rowx=i, colx=0)
-        print(pagenum)
         # for each word, assign a page
-        # index[pagenum].append(wordlist)
-
+        for word in wordArray:
+            lowerWord = word.lower().strip()
+            print(lowerWord, pagenum, word in wordArray)
+            if word in wordArray:
+                a = index.get(lowerWord, 'nope')
+            #index[lowerWord] = [pagenum]
+    sortedIndex = sorted(index)
+    print(sortedIndex)
 
 get_indexed_keywords(default_sheet)
-
-print(index)
